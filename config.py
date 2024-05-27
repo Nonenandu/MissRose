18d2392a86dd26e98c7bc86fa7a09f8e54957569
Nonenandu <noneofnone44@gmail.com>
2024-05-27 10:08:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "" # 🌚 get it from @botfather (telegram) by sending /newbot command.
-    API_ID =  # 🌚 Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "" # 🌚 Get it from my.telegram.org.
+    BOT_TOKEN = " 7369239817:AAG9rVm3lSRa0HrsMxG2aspI83fMb2D9lGQ " # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    API_ID = " 18090558 " # 🌚 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = " 497be964f330433334cda7404f3c5224 " # 🌚 Get it from my.telegram.org.
     
